f0de3951ae275234bc651fd320122e179ef88d37
Brad Frank <bradfrank@fastmail.com>
2021-06-29 12:13:27 -0400

    Refactor repository (#8)
    * **Removed Fedora support**
    * **The `inventory.yml` is a template so variables can be loaded from `group_vars`**
    * **No more GCP or kubectl segments in zsh prompt! Relies totally on aliases**
    * **Email address is now a passable parameter that is set in `inventory.yml`**
    * Managing ssh `config` and GitHub repos can be disabled with the `-w` flag
    * Fixed custom backups to run as handlers
    * No longer need to install `deb` packages from GitHub
    * Use `exa` in place of `ls` and added new aliases
    * Refactored `gcp-kubeconfigs` into `gcpkonfigs` to be more versatile
    * Python packages are no longer installed (should be relegated to virtualenvs)
    * Include a newer Solarized Light theme for `bat`
    * Configure BBEdit themes and cli tools for macOS
    * Tmux config includes options depending on the version installed
    * Converted dconf settings from files into Ansible dictionaries
    * Git repos default to using `master` as the default branch
    * Add `krename` function to cleanup kubectl context names
    * Removed references to O'Reilly in zsh config files and consolidated into `orm` script
    * Fixed `creates` path for Krew installer
    * Added more macOS squircle icons
    * Rearranged `files` directory
    * Added Art of Command Line reference repo

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Ansible playbook for bootstrapping macOS/Linux workstations and managing dotfiles.
 
-**Supported systems:** *macOS, Fedora, Ubuntu, Pop!_OS*
+**Supported systems:** *macOS, Ubuntu, Pop!_OS*
 
 ## Installing
 
@@ -20,15 +20,40 @@ wget https://raw.githubusercontent.com/bradleyfrank/ansible/main/run.sh
 
 ## Running
 
+This repo runs entirely local, no remote connections.
+
 ```text
-sh run.sh [-b | -d] [-g git_branch] | -h
-    -b  Run the bootstrap playbook (default)
-    -d  Run the dotfiles playbook
-    -g  Specify the git branch to run (default: 'main')
-    -h  Print this help menu and quit
+sh run.sh [-g git_branch] [-b | -d] [-e email] [-w] | -h
+  -g  Specify the git branch to run (default: main)
+  -b  Run the bootstrap playbook (default)
+  -d  Run the dotfiles playbook
+  -e  Email address for config files (default: username@hostname)
+  -w  Designate a work system; skip certain tasks (default: false)
+  -h  Print this help menu and quit
 ```
 
+* Creates an `inventory.yml` file with host-specific variables
 * Prompts for the Ansible vault password (saved to `~/.ansible/vault`)
 * Ansible is installed via `pip` on all systems
 * The `bootstrap` playbook requires `sudo` privileges for any system
-* Log into the Mac App Store to install apps via `mas` (skips if not authenticated)
+* Log in to the Mac App Store to install apps via `mas` (skips if not authenticated)
+
+### Inventory
+
+To regenerate `inventory.yml` anytime, run the following Ansible command, replacing the `{{ email }}` and `{{ work_system }}` placeholders.
+
+```shell
+ansible localhost \
+  --module-name ansible.builtin.template \
+  --args "src=playbooks/templates/inventory.yml.j2 dest=inventory.yml" \
+  --extra-vars "kernel_name=$(uname -s)" \
+  --extra-vars "email={{ email_address }}" \
+  --extra-vars "work_system={{ true|false }}"
+```
+
+### Work Systems
+
+Passing the `-w` flag skips the following tasks:
+
+* Managing `~/.ssh/config`
+* Cloning all personal GitHub repositories
--- run.sh
+++ run.sh
@@ -1,170 +1,151 @@
 #!/usr/bin/env sh
 
+set -o errexit
+
 # ----- global variables ----- #
 
-CHECKOUT_DIR="$(mktemp -d)"
-SYSTEM_TYPE="$(uname -s | tr '[:upper:]' '[:lower:]')"
-
-case "$SYSTEM_TYPE" in
-  darwin) SUDOERS_D="/private/etc/sudoers.d" ;;
-  linux)  SUDOERS_D="/etc/sudoers.d"         ;;
-esac
-
-HOMEBREW_URL="https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
-ANSIBLE_REPO_URL="https://github.com/bradleyfrank/ansible.git"
-ANSIBLE_REPO_BRANCH="main"
-ANSIBLE_REPO_PLAYBOOK="bootstrap"
+ANSIBLE_HOME="$HOME"/.ansible
+ANSIBLE_REPO_BRANCH=main
+ANSIBLE_REPO_PLAYBOOK=bootstrap
+ANSIBLE_REPO_URL=https://github.com/bradleyfrank/ansible.git
+ANSIBLE_VAULT="$ANSIBLE_HOME"/vault
+DOTFILES_DIR="$HOME"/.dotfiles
+EMAIL_ADDRESS="$(id -un)@$(uname -n)"
+HOMEBREW_URL=https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+TIMESTAMP=$(date +%F_%T | tr -d ':-' | tr '_' '-')
 
 
 # ----- functions ----- #
 
-trap cleanup INT
+trap 'cleanup; trap - EXIT; exit' EXIT INT HUP TERM
 
 cleanup() {
-  [ -e "$SUDOERS_D_TMP" ] && sudo rm -f "$SUDOERS_D_TMP"
-  [ -e "$CHECKOUT_DIR" ] && rm -rf "$CHECKOUT_DIR"
+  [ -f "$SUDOERS_D_TMP" ] && sudo rm -f "$SUDOERS_D_TMP"
 }
 
 usage() {
-    echo "sh run.sh [-b | -d] [-g git_branch] | -h"
+    echo "sh run.sh [-g git_branch] [-b | -d] [-e email] [-w] | -h"
+    echo "  -g  Specify the git branch to run (default: main)"
     echo "  -b  Run the bootstrap playbook (default)"
     echo "  -d  Run the dotfiles playbook"
-    echo "  -g  Specify the git branch to run (default: 'main')"
+    echo "  -e  Email address for config files (default: username@hostname)"
+    echo "  -w  Designate a work system; skip certain tasks (default: false)"
     echo "  -h  Print this help menu and quit"
 }
 
 create_tmp_sudoers() {
-  [ -e "$SUDOERS_D_TMP" ] && sudo rm -rf "$SUDOERS_D_TMP"
-  SUDOERS_D_TMP="${SUDOERS_D}/99-ansible-$(date +%F)"
-  sudo --validate --prompt "Enter sudo password: " # reset sudo timer for following command
+  SUDOERS_D_TMP="$SUDOERS_D/ansible.$TIMESTAMP"
   printf "%s ALL=(ALL) NOPASSWD: ALL\n" "$(id -un)" | sudo VISUAL="tee" visudo -f "$SUDOERS_D_TMP"
-  printf "\n" # insert newline for readability
 }
 
 create_vault_file() {
-  vaultfile="$HOME/.ansible/vault"
-  [ -s "$vaultfile" ] && return 0
-  [ ! -d "$HOME/.ansible" ] && mkdir "$HOME/.ansible"
+  [ -s "$ANSIBLE_VAULT" ] && return 0
 
-  vaultpw=$(
+  printf "%s" "$(
     exec < /dev/tty
-    tty_settings=$(stty -g) # save current tty settings
+    tty_settings=$(stty -g)
     trap 'stty "$tty_settings"' EXIT INT TERM
-    stty -echo || exit # disable terminal local echo
+    stty -echo || exit
     printf "Enter vault password: " > /dev/tty
-    IFS= read -r password; ret=$?
-    echo > /dev/tty # insert newline for readability
+    IFS= read -r password; rc=$?
+    echo > /dev/tty
     printf "%s\n" "$password"
-    exit "$ret"
-  )
+    exit "$rc"
+  )" > "$ANSIBLE_VAULT"
 
-  printf "%s" "$vaultpw" > "$vaultfile"
-  chmod 0400 "$vaultfile"
+  chmod 0400 "$ANSIBLE_VAULT"
 }
 
-keep_awake() {
-  case "$SYSTEM_TYPE" in
-    darwin)
-      kill "$(pgrep caffeinate)" >/dev/null 2>&1
-      (caffeinate -d -i -m -u &)
-      ;;
-    linux)
-      dconf write /org/gnome/desktop/session/idle-delay 'uint32 0'
-      dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-timeout "'nothing'"
-      ;;
-  esac
-}
+bootstrap_mac() {
+  SUDOERS_D=/private/etc/sudoers.d; create_tmp_sudoers
 
-not_supported() {
-  echo "Unsupported OS, aborting..." >&2
-  exit 1
-}
-
-bootstrap_os() {
-  case "$SYSTEM_TYPE" in
-    darwin) bootstrap_macos ;;
-    linux)  bootstrap_linux ;;
-    *)      not_supported   ;;
-  esac
-}
-
-bootstrap_macos() {
-  if [ ! -x /usr/local/bin/brew ]; then CI=1 /bin/bash -c "$(curl -fsSL "$HOMEBREW_URL")"
-  else softwareupdate --install --all
+  if ! pgrep caffeinate >/dev/null; then
+    (caffeinate -d -i -m -u &)
   fi
+
+  if [ ! -x /usr/local/bin/brew ]; then
+    CI=1 /bin/bash -c "$(curl -fsSL "$HOMEBREW_URL")"
+  else
+    softwareupdate --install --all
+  fi
+
   brew install python3 git
 }
 
 bootstrap_linux() {
-  case "$(sed -rn 's/^ID="?([a-z]+)"?/\1/p' /etc/os-release)" in
-    fedora)
-      sudo dnf clean all
-      sudo dnf makecache
-      sudo dnf upgrade -y
-      sudo dnf install -y python3 python3-pip git redhat-lsb-core
-      ;;
-    ubuntu|pop)
-      sudo apt-get clean
-      sudo apt-get update
-      sudo apt-get upgrade -y
-      sudo apt-get install -y python3 python3-pip git lsb-release
-      ;;
-    *) not_supported ;;
-  esac
+  SUDOERS_D=/etc/sudoers.d; create_tmp_sudoers
+
+  # shellcheck disable=SC1091
+  . /etc/os-release
+
+  if [ "$ID" != ubuntu ] && [ "$ID" != pop ]; then exit 1; fi
+
+  dconf write /org/gnome/desktop/session/idle-delay 'uint32 0'
+  dconf write /org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-timeout "'nothing'"
+
+  sudo systemctl stop packagekit
+  sudo apt-get clean
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt-get install -y python3 python3-pip git lsb-release
 }
 
-pre_ansible_run() {
+bootstrap_ansible() {
+  PATH="$PATH:$(python3 -m site --user-base)/bin"; export PATH
   python3 -m pip install --user ansible docker github3.py
-  PATH="$PATH:$(python3 -m site --user-base)/bin"
-  export PATH
 
-  git clone "$ANSIBLE_REPO_URL" "$CHECKOUT_DIR"
-  cd "$CHECKOUT_DIR" >/dev/null 2>&1 || return 1
+  [ -d "$DOTFILES_DIR" ] && mv "$DOTFILES_DIR" "$DOTFILES_DIR.$TIMESTAMP"
+  git clone "$ANSIBLE_REPO_URL" "$DOTFILES_DIR"
+
+  cd "$DOTFILES_DIR"
   git checkout "$ANSIBLE_REPO_BRANCH"
-  cd - >/dev/null 2>&1 || return 1
 
-  ansible-galaxy collection install -r "$CHECKOUT_DIR"/requirements.yml
+  ansible localhost \
+    --module-name ansible.builtin.template \
+    --args "src=playbooks/templates/inventory.yml.j2 dest=inventory.yml" \
+    --extra-vars "kernel_name=$(uname -s)" \
+    --extra-vars "email=$EMAIL_ADDRESS" \
+    --extra-vars "work_system=${WORK_SYSTEM:-false}"
+
+  ansible-galaxy collection install -r requirements.yml
+
+  cd -
 }
 
-ansible_playbook() {
+ansible_run() {
+  cd "$DOTFILES_DIR"
   case "$ANSIBLE_REPO_PLAYBOOK" in
     bootstrap) ansible-playbook --ask-become-pass playbooks/bootstrap.yml ;;
     dotfiles)  ansible-playbook playbooks/dotfiles.yml ;;
   esac
-}
-
-ansible_run() {
-  cd "$CHECKOUT_DIR" >/dev/null 2>&1 || return 1
-  if ansible_playbook; then
-    cd - >/dev/null 2>&1 || return 1
-    [ -e "$HOME"/.dotfiles ] && rm -rf "$HOME"/.dotfiles
-    mv "$CHECKOUT_DIR" "$HOME"/.dotfiles
-    return 0
-  else
-    cd - >/dev/null 2>&1 || return 1
-    mv "$CHECKOUT_DIR" "$HOME"/.ansible_"$(date +%F%T | tr -d ':-')"
-    return 1
-  fi
+  cd -
 }
 
 
 # ----- main ----- #
 
-while getopts ':bdg:h' opt; do
+while getopts ':bde:g:wh' opt; do
   case "$opt" in
-    b) ANSIBLE_REPO_PLAYBOOK="bootstrap" ;;
-    d) ANSIBLE_REPO_PLAYBOOK="dotfiles"  ;;
-    g) ANSIBLE_REPO_BRANCH="$OPTARG"     ;;
-    h) usage ; exit 0 ;;
-    *) usage ; exit 1 ;;
+    b) ANSIBLE_REPO_PLAYBOOK=bootstrap ;;
+    d) ANSIBLE_REPO_PLAYBOOK=dotfiles  ;;
+    e) EMAIL_ADDRESS="$OPTARG"         ;;
+    g) ANSIBLE_REPO_BRANCH="$OPTARG"   ;;
+    w) WORK_SYSTEM=true ;;
+    h) usage; exit 0    ;;
+    *) usage; exit 1    ;;
   esac
 done
 
-case "$ANSIBLE_REPO_PLAYBOOK" in
-  bootstrap) create_tmp_sudoers ; create_vault_file ; keep_awake ; bootstrap_os ;;
-  dotfiles)  create_vault_file ;;
-  *)         exit 1 ;;
-esac
+[ ! -d "$ANSIBLE_HOME" ] && mkdir "$ANSIBLE_HOME"
+create_vault_file
 
-pre_ansible_run ; [ $? = 1 ] && { cleanup ; exit 1 ; }
-ansible_run ; rc=$? ; cleanup ; exit $rc
+if [ "$ANSIBLE_REPO_PLAYBOOK" = bootstrap ]; then
+  case $(uname -s) in
+    Darwin) bootstrap_mac ;;
+    Linux)  bootstrap_linux ;;
+    *)      exit 1 ;;
+  esac
+fi
+
+bootstrap_ansible
+ansible_run
