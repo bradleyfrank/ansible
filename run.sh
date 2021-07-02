d74874be8af2f0d9e823e5f07ee01482d68aee41
Bradley Frank <bradfrank@fastmail.com>
2021-07-02 13:01:41 -0400

    Create vault file and inventory in playbook

--- run.sh
+++ run.sh
@@ -4,11 +4,9 @@ set -o errexit
 
 # ----- global variables ----- #
 
-ANSIBLE_HOME="$HOME"/.ansible
 ANSIBLE_REPO_BRANCH=main
 ANSIBLE_REPO_PLAYBOOK=bootstrap
 ANSIBLE_REPO_URL=https://github.com/bradleyfrank/ansible.git
-ANSIBLE_VAULT="$ANSIBLE_HOME"/vault
 DOTFILES_DIR="$HOME"/.dotfiles
 EMAIL_ADDRESS="$(id -un)@$(uname -n)"
 HOMEBREW_URL=https://raw.githubusercontent.com/Homebrew/install/master/install.sh
@@ -38,30 +36,10 @@ create_tmp_sudoers() {
   printf "%s ALL=(ALL) NOPASSWD: ALL\n" "$(id -un)" | sudo VISUAL="tee" visudo -f "$SUDOERS_D_TMP"
 }
 
-create_vault_file() {
-  [ -s "$ANSIBLE_VAULT" ] && return 0
-
-  printf "%s" "$(
-    exec < /dev/tty
-    tty_settings=$(stty -g)
-    trap 'stty "$tty_settings"' EXIT INT TERM
-    stty -echo || exit
-    printf "Enter vault password: " > /dev/tty
-    IFS= read -r password; rc=$?
-    echo > /dev/tty
-    printf "%s\n" "$password"
-    exit "$rc"
-  )" > "$ANSIBLE_VAULT"
-
-  chmod 0400 "$ANSIBLE_VAULT"
-}
-
 bootstrap_mac() {
   SUDOERS_D=/private/etc/sudoers.d; create_tmp_sudoers
 
-  if ! pgrep caffeinate >/dev/null; then
-    (caffeinate -d -i -m -u &)
-  fi
+  pgrep caffeinate >/dev/null || (caffeinate -d -i -m -u &)
 
   if [ ! -x /usr/local/bin/brew ]; then
     CI=1 /bin/bash -c "$(curl -fsSL "$HOMEBREW_URL")"
@@ -100,14 +78,13 @@ bootstrap_ansible() {
   cd "$DOTFILES_DIR" >/dev/null
   git checkout "$ANSIBLE_REPO_BRANCH"
 
-  ansible localhost \
-    --module-name ansible.builtin.template \
-    --args "src=playbooks/templates/inventory.yml.j2 dest=inventory.yml" \
-    --extra-vars "kernel_name=$(uname -s)" \
+  ansible-galaxy collection install -r requirements.yml
+
+  ANSIBLE_CONFIG=setup.cfg \
+  ansible-playbook playbooks/setup.yml \
+    --inventory setup.yml \
     --extra-vars "email=$EMAIL_ADDRESS" \
     --extra-vars "work_system=${WORK_SYSTEM:-false}"
-
-  ansible-galaxy collection install -r requirements.yml
 
   cd - >/dev/null
 }
@@ -135,9 +112,6 @@ while getopts ':bde:g:wh' opt; do
     *) usage; exit 1    ;;
   esac
 done
-
-[ ! -d "$ANSIBLE_HOME" ] && mkdir "$ANSIBLE_HOME"
-create_vault_file
 
 if [ "$ANSIBLE_REPO_PLAYBOOK" = bootstrap ]; then
   case $(uname -s) in
