0f748d660fd8064faee5dbf6c7217efd53386579
Bradley Frank <bradfrank@fastmail.com>
2025-04-21 10:09:00 -0400

    Work-In-Progress

--- roles/vim/files/nvim/lua/plugins/init.lua
+++ roles/vim/files/nvim/lua/plugins/init.lua
@@ -4,22 +4,21 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  		  "vim", "lua", "vimdoc", "html", "css"
+  		},
+  	},
+  },
+  {
+    "folke/which-key.nvim"
+  },
 }
