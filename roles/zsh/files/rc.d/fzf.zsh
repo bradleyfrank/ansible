863c6702517355985b3482471366bfccde839f88
Bradley Frank <bradfrank@fastmail.com>
2025-05-17 11:16:46 -0400

    feat: re-apply Solarized Light theme across dotfiles

--- roles/zsh/files/rc.d/fzf.zsh
+++ roles/zsh/files/rc.d/fzf.zsh
@@ -1,30 +1,4 @@
-typeset -a fzf_default_colors=(
-  "border:#dfdad9"
-  "header:#286983"
-  "info:#56949f"
-  "gutter:#faf4ed"
-  "prompt:#797593"
-  "pointer:#907aa9"
-  "marker:#b4637a"
-  "spinner:#ea9d34"
-  "fg:#797593"
-  "fg+:#575279"
-  "bg:#faf4ed"
-  "bg+:#f2e9e1"
-  "hl:#d7827e"
-  "hl+:#d7827e"
-)
-
-typeset -a fzf_default_opts=(
-  "--color=${(j',')fzf_default_colors}"
-  "--info=inline"
-  "--select-1"
-  "--exit-0"
-  "--reverse"
-  "--height=~100%"
-)
-
-typeset -a eza_opts=(
+typeset -a __eza_opts=(
   "--almost-all"
   "--long"
   "--octal-permissions"
@@ -36,8 +10,7 @@ typeset -a eza_opts=(
   "--time-style=relative"
 )
 
-export FZF_DEFAULT_OPTS="${(j' ')fzf_default_opts}"
+export FZF_DEFAULT_OPTS_FILE="${HOME}/.config/fzf/config"
 
 zstyle ':fzf-tab:*' switch-group '<' '>'
-zstyle ':fzf-tab:*' fzf-flags --color='light' --border='rounded' --height='~100%'
-zstyle ':fzf-tab:complete:cd:*' fzf-preview "eza ${(j' ')eza_opts} $realpath"
+zstyle ':fzf-tab:complete:cd:*' fzf-preview "eza ${(j' ')__eza_opts} $realpath"
