ae11207cddc40c973562af1668914e4b81109bce
Bradley Frank <bradfrank@fastmail.com>
2025-05-22 18:26:48 -0400

    feat(nvim): Force sh scripts to use Bash syntax

--- roles/vim/files/nvim/init.lua
+++ roles/vim/files/nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
+vim.g.is_bash = 1
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
